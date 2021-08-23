9a041f1f62a20befaf36ccd12bc28f7c57cb52c8
ThisIsMunna <authenticmunna@gmail.com>
2021-08-23 18:11:27 +0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="bg-gray-500 min-h-screen items-center justify-center flex flex-col">
+        <button className="p-5">Start</button>
+      <div className="bg-red-500 h-5 w-5">
+
+      </div>
+      </div>
+
     </div>
   );
 }
